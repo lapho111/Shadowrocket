1ce1b9120975aef8e6a5aecefd7c8c3c825e4a2e
lapho111 <lap17022005@gmail.com>
2025-02-22 16:50:47 +0700

    Create Alight Motion.js

---
+++ Alight Motion.js
@@ -0,0 +1,42 @@
+/******************************
+📌 Tác Giả：Lạp Hộ  
+📌 Cập Nhật：2025-2-17  
+📌 Liên Lạc：Zalo: 0886632736  
+📌 Face Book: https://www.facebook.com/lapho111
+*******************************/
+
+objc = {
+  "result": {
+    "result": "success",
+    "msTime": 1759344000000,  
+    "accountCreatedMillis": null,
+    "licenses": [
+      {
+        "benefits": [
+          "RemoveWatermark",
+          "MemberEffects",
+          "ProjectPackageSharing",
+          "FutureMemberFeatures",
+          "AdvancedEasing",
+          "CameraObjects",
+          "LayerParenting",
+          "CloudStorageLowTier"
+        ],
+        "type": "subscription",
+        "store": "apple_app_store",
+        "autoRenewing": true,
+        "orderNumber": "300001752007005",
+        "productId": "alightcreative.motion.1y_t60_1w",
+        "period": "1y",
+        "label": null,
+        "details": null,
+        "expires": 4102444800000,
+        "valid": true,
+        "linkStatus": "linked-current"
+      }
+    ],
+    "warnings": []
+  }
+};
+
+$done({ body: JSON.stringify(objc) });
