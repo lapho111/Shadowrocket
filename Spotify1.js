fd44816a15a1f343dca2451d37ef4f39937ae00b
Lạp Hộ <lap17022005@gmail.com>
2025-02-27 15:57:07 +0700

    Update Spotify1.js

--- Spotify1.js
+++ Spotify1.js
@@ -1,9 +1,16 @@
-console.log(`spotify-json-2025-01-01`);
+/******************************
+📌 Tác Giả：Lạp Hộ  
+📌 Cập Nhật：2025-2-17  
+📌 Liên Lạc：Zalo: 0886632736  
+📌 Face Book: https://www.facebook.com/lapho111
+*******************************/
+
+console.log(`spotify-json-2023.06.13`);
 let url = $request.url;
 if (url.includes('platform=iphone')) {
     url = url.replace(/platform=iphone/, 'platform=ipad');
 } else {
-    console.log('Không cần xử lý');
+    console.log('无需处理');
 }
 $done({
     url
