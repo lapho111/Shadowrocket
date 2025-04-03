4a9b1803972dd171c929c555d61ae4c795d3c1d6
Lạp Hộ <lap17022005@gmail.com>
2025-04-03 09:17:26 +0700

    Update and rename Spotify1.js to Spotify-json.js

--- Spotify-json.js
+++ Spotify-json.js
@@ -1,9 +1,9 @@
-console.log(`spotify-json-2025.04.03`);
+console.log(`spotify-json-2023.06.13`);
 let url = $request.url;
 if (url.includes('platform=iphone')) {
     url = url.replace(/platform=iphone/, 'platform=ipad');
 } else {
-    console.log('No treatment required');
+    console.log('无需处理');
 }
 $done({
     url
