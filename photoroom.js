2abf68a6c34ec6e28921cdab34b54c9f9300af38
Lạp Hộ <lap17022005@gmail.com>
2025-04-20 15:47:01 +0700

    Update photoroom.js

--- photoroom.js
+++ photoroom.js
@@ -1,19 +1,24 @@
-var ojbk = JSON.parse($response.body);
-ojbk['subscriber']['subscriptions'] = {
-    'business': {
-        'expires_date': '2033-02-23T03:33:33Z',
-        'product_identifier': 'com.background.business.yearly',
-        'purchase_date': '2023-02-23T03:33:33Z'
+// PhotoRoom Premium Unlock (by modifying RevenueCat response)
+let response = JSON.parse($response.body);
+
+response.subscriber = {
+  entitlements: {
+    business: {
+      expires_date: "2099-09-09T09:09:09Z",
+      product_identifier: "com.background.business.yearly",
+      purchase_date: "2022-09-09T09:09:09Z"
     }
-};
-ojbk['subscriber']['first_seen'] = '2023-02-23T03:33:33Z';
-ojbk['subscriber']['entitlements'] = {
-    'com.background.business.yearly': {
-        'expires_date': '2033-02-23T03:33:33Z',
-        'original_purchase_date': '2023-02-23T03:33:33Z',
-        'purchase_date': '2023-02-23T03:33:33Z',
-        'ownership_type': 'PURCHASED',
-        'store': 'app_store'
+  },
+  first_seen: "2023-02-23T03:33:33Z",
+  subscriptions: {
+    "com.background.business.yearly": {
+      expires_date: "2099-09-09T09:09:09Z",
+      original_purchase_date: "2022-09-09T09:09:09Z",
+      purchase_date: "2022-09-09T09:09:09Z",
+      ownership_type: "PURCHASED",
+      store: "app_store"
     }
+  }
 };
-$done({'body': JSON.stringify(ojbk)});
+
+$done({ body: JSON.stringify(response) });
